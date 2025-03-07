2adb9fe4fdb64cf888e7bb647d5cede34e17dd40
Peter Plantinga <plantinga.peter@proton.me>
2025-03-07 14:38:31 -0500

    Add/update LLM recipes

--- recipes/ADReSSo/diagnosis/train_llm.py
+++ recipes/ADReSSo/diagnosis/train_llm.py
@@ -54,24 +54,16 @@ class ParkinsonBrain(sb.core.Brain):
         # Predict
         feats = self.modules.compute_features(tokens, attn_mask)
         embeddings = self.modules.embedding_model(feats)
-
-        # Add vocal embeddings
-        if self.hparams.concat_vocal:
-            wav, wavlens = batch.sig
-            vocal_feats = self.modules.vocal_features(wav)
-            vocal_embed = self.modules.vocal_embedding_model(vocal_feats)
-            embeddings = torch.cat((embeddings, vocal_embed), dim=-1)
-
-        # Outputs
         outputs = self.modules.classifier(embeddings)
 
+        # Outputs
         return outputs, lens
 
     def compute_objectives(self, outputs, batch, stage):
         """Computes the loss using patient-type as label."""
 
         # Get predictions and labels
-        labels, _ = batch.patient_type_encoded
+        labels, _ = batch.dx_encoded
         outputs, lens = outputs
 
         # Concatenate labels in case of wav_augment
@@ -80,13 +72,13 @@ class ParkinsonBrain(sb.core.Brain):
 
         # Compute loss
         if stage == sb.Stage.TRAIN:
-            loss = self.hparams.bce_loss(outputs, labels, weight=batch.weight)
+            loss = self.hparams.bce_loss(outputs, labels)#, weight=batch.weight)
 
         # Validation / Test
         else:
             probs = torch.sigmoid(outputs.view(-1))
             self.error_metrics.append(batch.id, probs, labels.view(-1))
-            self.error_metrics.info_dicts.extend(batch.info_dict)
+            #self.error_metrics.info_dicts.extend(batch.info_dict)
 
             # Use unweighted, unsmoothed score for comparable results across hparams
             loss = binary_cross_entropy(probs, labels.view(-1).float())
@@ -99,7 +91,7 @@ class ParkinsonBrain(sb.core.Brain):
             self.error_metrics = self.hparams.error_stats()
 
             # Add this list so we can store the info dict
-            self.error_metrics.info_dicts = []
+            #self.error_metrics.info_dicts = []
 
     def on_stage_end(self, stage, stage_loss, epoch=None):
         """Gets called at the end of an epoch."""
@@ -113,28 +105,25 @@ class ParkinsonBrain(sb.core.Brain):
 
             # Generate overall metrics, using stored threshold for test set
             avg_threshold = None if stage == sb.Stage.VALID else self.avg_threshold
-            #avg_threshold = 0.5
             metrics_comb_avg = self.metrics_by_category(
                 combined_avg, target_category=None, threshold=avg_threshold
             )
 
             # Log overall metrics
-            chunk_stats = self.summarize_metrics(
-                self.error_metrics, self.hparams.threshold
-            )
+            chunk_stats = self.summarize_metrics(self.error_metrics, threshold=None)
             stage_stats.update({f"chunk_{k}": v for k, v in chunk_stats.items()})
             stage_stats.update(
                 {f"comb_avg_{k}": v for k, v in metrics_comb_avg["overall"].items()}
             )
 
             # Log metrics split by given categories
-            for category in self.hparams.metric_categories:
-                threshold = metrics_comb_avg["overall"]["threshold"]
-                cat_metrics = self.metrics_by_category(
-                    combined_scores=combined_avg, target_category=category, threshold=threshold
-                )
-                logger.info(f"Comb avg breakdown by {category}")
-                logger.info(pprint.pformat(cat_metrics, indent=2, compact=True, width=100))
+            #for category in self.hparams.metric_categories:
+            #    threshold = metrics_comb_avg["overall"]["threshold"]
+            #    cat_metrics = self.metrics_by_category(
+            #        combined_scores=combined_avg, target_category=category, threshold=threshold
+            #    )
+            #    logger.info(f"Comb avg breakdown by {category}")
+            #    logger.info(pprint.pformat(cat_metrics, indent=2, compact=True, width=100))
 
             # Dump metrics to file only on test
             if stage == sb.Stage.TEST:
@@ -197,28 +186,29 @@ class ParkinsonBrain(sb.core.Brain):
         ids = self.error_metrics.ids
         scores = self.error_metrics.scores
         labels = self.error_metrics.labels
-        info_dicts = self.error_metrics.info_dicts
+        #info_dicts = self.error_metrics.info_dicts
 
         combined_scores = {}
-        for utt_id, score, label, info_dict in zip(ids, scores, labels, info_dicts):
+        #for utt_id, score, label, info_dict in zip(ids, scores, labels, info_dicts):
+        for utt_id, score, label in zip(ids, scores, labels):
             if utt_id not in combined_scores:
                 combined_scores[utt_id] = {
-                    "scores": [round(score.item(), 4)],
+                    "scores": [round(score.item(), 3)],
                     "label": label.item(),
-                    **info_dict,
+                    #**info_dict,
                 }
             else:
-                combined_scores[utt_id]["scores"].append(round(score.item(), 4))
+                combined_scores[utt_id]["scores"].append(round(score.item(), 3))
 
         # For now just take the average or max. Perhaps do something fancier later
         for utt_id in combined_scores:
             scores = combined_scores[utt_id]["scores"]
             if how == "avg":
                 combined_scores[utt_id]["combined"] = round(
-                    sum(scores) / len(scores), 4
+                    sum(scores) / len(scores), 3
                 )
             elif how == "max":
-                combined_scores[utt_id]["combined"] = round(max(scores), 4)
+                combined_scores[utt_id]["combined"] = round(max(scores), 3)
             else:
                 raise ValueError("Expected 'avg' or 'max'")
 
@@ -268,55 +258,38 @@ def dataio_prep(hparams):
     # of the observed label a unique index (e.g, 'hc': 0, 'pd': 1, ..)
     label_encoder = sb.dataio.encoder.CategoricalEncoder()
     label_encoder.expect_len(2)
-    label_encoder.enforce_label("PD", 1)
-    label_encoder.enforce_label("HC", 0)
-
-    @sb.utils.data_pipeline.takes("wav")
-    @sb.utils.data_pipeline.provides("sig")
-    def wav_pipeline(wav):
-        sig, fs = torchaudio.load(wav, num_frames=16000 * 120)
-        yield sig.squeeze(0)
+    label_encoder.enforce_label("ProbableAD", 1)
+    label_encoder.enforce_label("Control", 0)
 
     # Define transcript pipeline (without augmentations):
-    @sb.utils.data_pipeline.takes("transcript", "manual")
+    @sb.utils.data_pipeline.takes("transcript")
     @sb.utils.data_pipeline.provides("tokens", "attn_mask")
-    def transcript_eval_pipeline(transcript, manual):
-
-        if manual != "":
-            transcript = manual
+    def transcript_eval_pipeline(transcript):
         tokens = hparams["compute_features"].tokenizer(transcript, return_tensors="pt")
         yield tokens["input_ids"][0]
         yield tokens["attention_mask"][0]
 
     # Transcript train pipeline (with augmentations):
-    @sb.utils.data_pipeline.takes("transcript", "translation", "manual")
+    @sb.utils.data_pipeline.takes("transcript")#, "translation")
     @sb.utils.data_pipeline.provides("tokens", "attn_mask")
-    def transcript_train_pipeline(transcript, translation, manual):
+    def transcript_train_pipeline(transcript):#, translation):
         #print(transcript)
         #print(translation)
 
-        if manual != "":
-            transcript = manual
-
-        s = np.random.binomial(1, hparams["input_translate_prob"])
-        words = translation.split(" ") if s else transcript.split(" ")
-
-        #print(words)
+        #s = np.random.binomial(1, hparams["input_translate_prob"])
+        #words = translation.split(" ") if s else transcript.split(" ")
+        words = transcript.split(" ")
 
         # Random word drop
         drop_count = int(hparams["input_drop_frac"] * len(words))
         drop_choices = np.random.choice(len(words), drop_count, replace=False)
         words = [w for i, w in enumerate(words) if i not in drop_choices]
 
-        #print(words)
-
         # Random word swap
         swap_count = int(hparams["input_swap_frac"] * len(words)) // 2
         swap_choices = np.random.choice(len(words), swap_count * 2, replace=False)
         for i, j in zip(swap_choices[:swap_count], swap_choices[swap_count:]):
             words[i], words[j] = words[j], words[i]
-
-        #print(words)
 
         # Convert to tokens
         transcript = " ".join(words)
@@ -325,31 +298,27 @@ def dataio_prep(hparams):
         yield tokens["attention_mask"][0]
 
     # Define label pipeline:
-    @sb.utils.data_pipeline.takes("info_dict")
-    @sb.utils.data_pipeline.provides(
-        "patient_type", "patient_type_encoded", "weight", "to_balance"
-    )
-    def label_pipeline(info_dict):
+    @sb.utils.data_pipeline.takes("dx")
+    @sb.utils.data_pipeline.provides("dx_encoded")#, "weight", "to_balance"
+    def label_pipeline(dx):
         """Defines the pipeline to process the patient type labels.
         Note that we have to assign a different integer to each class
         through the label encoder.
         """
-        yield info_dict["ptype"]
-        patient_type_encoded = label_encoder.encode_label_torch(info_dict["ptype"])
-        yield patient_type_encoded
+        yield label_encoder.encode_label_torch(dx)
 
         # Weight PD less since there's more in the data
         #weight = 0.7 if patient_type_encoded else 1.5
         #weight *= 0.7 if info_dict["sex"] == "M" else 1.5
         #weight *= 0.7 if info_dict["lang"] == "fr" else 1.5
-        weight = 1.0
-        yield weight
+        #weight = 1.0
+        #yield weight
 
         # Balance on ptype
-        balance = info_dict["ptype"]
-        balance += "_" + info_dict["sex"]
+        #balance = info_dict["ptype"]
+        #balance += "_" + info_dict["sex"]
         #balance += "_FR" if info_dict["lang"] == "fr" else "_EN/O"
-        yield balance
+        #yield balance
 
     # Define datasets. We also connect the dataset with the data processing
     # functions defined above.
@@ -360,38 +329,27 @@ def dataio_prep(hparams):
         "test": hparams["test_annotation"],
     }
 
-    out_keys = ["id", "tokens", "attn_mask", "patient_type_encoded", "info_dict", "weight", "to_balance", "sig"]
+    #out_keys = ["id", "tokens", "attn_mask", "patient_type_encoded", "info_dict", "weight", "to_balance"]
+    out_keys = ["id", "tokens", "attn_mask", "dx_encoded"]
     for dataset, json_path in train_info.items():
         if dataset == "train":
-            dynamic_items = [wav_pipeline, transcript_train_pipeline, label_pipeline]
+            dynamic_items = [transcript_train_pipeline, label_pipeline]
         else:
-            dynamic_items = [wav_pipeline, transcript_eval_pipeline, label_pipeline]
+            dynamic_items = [transcript_eval_pipeline, label_pipeline]
         datasets[dataset] = sb.dataio.dataset.DynamicItemDataset.from_json(
             json_path=json_path, dynamic_items=dynamic_items, output_keys=out_keys,
         )
 
-    # Remove keys from training data for e.g. training only on men
-    # Also remove very short utterances
-    for key, values in hparams["train_keep_keys"].items():
-        datasets["train"] = datasets["train"].filtered_sorted(
-            key_test={
-                "info_dict": lambda x: x[key] in values,
-                #"tokens": lambda x: len(x) > 15, # Using tokens here causes some warning from the tokenizer.
-                "transcript": lambda x: len(x) > 50,
-            },
-        )
-    for key, values in hparams["test_keep_keys"].items():
-        for dataset in ["valid", "test"]:
-            datasets[dataset] = datasets[dataset].filtered_sorted(
-                key_test={"info_dict": lambda x: x[key] in values},
-            )
-
-    hparams["train_dataloader_options"]["sampler"] = BalancingDataSampler(
-        dataset=datasets["train"],
-        key="to_balance",
-        num_samples=hparams["samples_per_epoch"],
-        replacement=True,
+    # remove very short utterances
+    datasets["train"] = datasets["train"].filtered_sorted(
+        key_test={"transcript": lambda x: len(x) > 50},
     )
+    #hparams["train_dataloader_options"]["sampler"] = BalancingDataSampler(
+    #    dataset=datasets["train"],
+    #    key="to_balance",
+    #    num_samples=hparams["samples_per_epoch"],
+    #    replacement=True,
+    #)
     #hparams["train_dataloader_options"]["sampler"] = ReproducibleWeightedRandomSampler(
     #    weights=[d["weight"] for d in datasets["train"]],
     #    num_samples=hparams["samples_per_epoch"],
@@ -415,11 +373,12 @@ if __name__ == "__main__":
     with open(hparams_file) as fin:
         hparams = load_hyperpyyaml(fin, overrides)
 
-    # Dataset prep (parsing KCL MDVR and annotation into json files)
-    from prepare_neuro import prepare_neuro
+    # Dataset prep 
+    from prepare_adresso import prepare_adresso
 
+    print(hparams["transcript_folder"])
     sb.utils.distributed.run_on_main(
-        prepare_neuro,
+        prepare_adresso,
         kwargs={
             "data_folder": hparams["data_folder"],
             "train_annotation": hparams["train_annotation"],
