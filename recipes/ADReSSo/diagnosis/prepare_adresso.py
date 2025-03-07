c5767520ccda3aa532c08e0130f666683237079e
Peter Plantinga <plantinga.peter@proton.me>
2025-03-07 14:51:04 -0500

    Add transcripts to addresso

--- recipes/ADReSSo/diagnosis/prepare_adresso.py
+++ recipes/ADReSSo/diagnosis/prepare_adresso.py
@@ -8,7 +8,7 @@ import pandas
 
 
 def prepare_adresso(
-    data_folder, train_annotation, test_annotation, valid_annotation, chunk_size
+    data_folder, train_annotation, test_annotation, valid_annotation, chunk_size, transcript_folder=None
 ):
     assert os.path.exists(data_folder), "Data folder not found"
     #if os.path.exists(train_annotation):
@@ -19,6 +19,10 @@ def prepare_adresso(
     train_gt = read_csv(data_folder, "train")
     test_gt = read_csv(data_folder, "test-dist")
 
+    train_transcripts = read_transcripts(transcript_folder, "train.json")
+    valid_transcripts = read_transcripts(transcript_folder, "valid.json")
+    test_transcripts = read_transcripts(transcript_folder, "test.json")
+
     # Separate out validation set
     valid_pids = ["adrso247", "adrso248", "adrso249", "adrso250", "adrso253", "adrso257", "adrso259", "adrso260", "adrso261", "adrso262"]
 
@@ -26,9 +30,9 @@ def prepare_adresso(
     train_gt = train_gt[~train_gt["ID"].isin(valid_pids)]
 
     # Create json manifests
-    create_json(train_annotation, train_gt, chunk_size, overlap=0)
-    create_json(test_annotation, test_gt, chunk_size)
-    create_json(valid_annotation, valid_gt, chunk_size)
+    create_json(train_annotation, train_gt, chunk_size, train_transcripts, overlap=0)
+    create_json(test_annotation, test_gt, chunk_size, test_transcripts)
+    create_json(valid_annotation, valid_gt, chunk_size, valid_transcripts)
 
 
 def read_csv(data_folder, subset):
@@ -37,7 +41,14 @@ def read_csv(data_folder, subset):
     return df
 
 
-def create_json(json_file, ground_truth, chunk_size, overlap=None):
+def read_transcripts(transcript_folder, filename):
+    if transcript_folder is None:
+        return None
+    with open(pathlib.Path(transcript_folder) / filename) as f:
+        return json.load(f)
+
+
+def create_json(json_file, ground_truth, chunk_size, transcripts=None, overlap=None):
     hop_size = chunk_size / 2 if overlap is None else chunk_size - overlap
     json_dict = {}
 
@@ -46,16 +57,23 @@ def create_json(json_file, ground_truth, chunk_size, overlap=None):
         audioinfo = torchaudio.info(row["path"])
         duration = audioinfo.num_frames / audioinfo.sample_rate
 
-        # Write chunks to dict
-        max_start = max(duration - hop_size, 1)
-        for i, start in enumerate(np.arange(0, max_start, hop_size)):
-            chunk_duration = min(chunk_size, duration - i * hop_size)
-            json_dict[f"{row['ID']}_{i}"] = {
-                "wav": str(row["path"]),
-                "start": start,
-                "duration": chunk_duration,
-                "dx": row["Dx"],
+        if transcripts is not None:
+            key = row["path"].name
+            transcript = transcripts[key] if key in transcripts else ""
+            json_dict[row['ID']] = {
+                "wav": str(row["path"]), "dx": row["Dx"], "transcript": transcript
             }
+        else:
+            # Write chunks to dict
+            max_start = max(duration - hop_size, 1)
+            for i, start in enumerate(np.arange(0, max_start, hop_size)):
+                chunk_duration = min(chunk_size, duration - i * hop_size)
+                json_dict[f"{row['ID']}_{i}"] = {
+                    "wav": str(row["path"]),
+                    "start": start,
+                    "duration": chunk_duration,
+                    "dx": row["Dx"],
+                }
 
     # Writing the dictionary to the json file
     with open(json_file, mode="w") as json_f:
